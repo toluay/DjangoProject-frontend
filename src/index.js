7b297d642baf437af67803b57cdb9eaa2fabb361
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-10 16:38:52 +0100

    Allert provider and template imported from react- alert , react-transition-group package

--- src/index.js
+++ src/index.js
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
 import { Provider } from 'react-redux';
+
 import store from '../store';
+
+
+
+
+
+// Alert Options
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center',
+  };
+
+
 
 ReactDOM.render(
     <Provider store={store}>
+          <AlertProvider template={AlertTemplate} {...alertOptions}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </AlertProvider>
   </Provider>,
   document.getElementById('root')
 );
