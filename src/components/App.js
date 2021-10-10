5533e484a7746fe27e6a4ae8d049401c0048c952
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-10 18:47:27 +0100

    index.js refacor and the App js also refactor to export and the indexjs do the dom rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,65 @@
-import  { Component } from 'react';
+import React from 'react';
 
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+
+
+
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+import Header from './layout/Header';
 import Dashboard from './customers/Dashboard';
+import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+import PrivateRoute from './common/PrivateRoute';
 
-class App extends Component {
+
+import { Provider } from 'react-redux';
+
+import store from '../store';
+import { loadUser } from '../actions/auth';
 
 
-  
-      render() {
-          return (<div> 
-            <Dashboard/>
-            </div>
-          )}
+// Alert Options
+const alertOptions = {
+    timeout: 3000,
+    position: 'top center',
+  };
+
+  class App extends Component {
+
+
+    componentDidMount() {
+      store.dispatch(loadUser());
+    }
+
+render(){
+    <Provider store={store}>
+          <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Router>
+          <Fragment>
+
+
+  <Header/>
+  <Alerts />
+
+  <div className="container">
+  <Switch>
+    <PrivateRoute exact path="/" component={Dashboard} />
+    <Route exact path="/register" component={Register} />
+    <Route exact path="/login" component={Login} />
+</Switch>
+    </div>
+
+
+
+    </Fragment>
+   
+ </Router>
+  </AlertProvider>
+  </Provider>
 }
-export default App;
+  }
+  export default App; 
--- src/index.js
+++ src/index.js
@@ -1,61 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-
-
-
-import { Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
-
-import Header from './layout/Header';
-import Dashboard from './customers/Dashboard';
-import Alerts from './layout/Alerts';
-import Login from './accounts/Login';
-import Register from './accounts/Register';
-
-
-import { Provider } from 'react-redux';
-
-import store from '../store';
-
-
-
-
-
-// Alert Options
-const alertOptions = {
-    timeout: 3000,
-    position: 'top center',
-  };
-
-
-
-ReactDOM.render(
-    <Provider store={store}>
-          <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Router>
-          <Fragment>
-
-
-  <Header/>
-  <Alerts />
-
-  <div className="container">
-  <Switch>
-    <Route exact path="/" component={Dashboard} />
-    <Route exact path="/register" component={Register} />
-    <Route exact path="/login" component={Login} />
-</Switch>
-    </div>
-
-
-
-    </Fragment>
-   
- </Router>
-  </AlertProvider>
-  </Provider>,
+import App from './components/App'
+  
+  
+ReactDOM.render(<App/>,
   document.getElementById('root')
 );
 
