98b03a43b474fe45b8bb51895cde159b0009e45c
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-10 16:49:45 +0100

    error state monitor, error reducer added with the type -action to be  used in the customer actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import customers from './customers';
+import errors from './errors';
 
 
 export default combineReducers({
     customers,
+    errors,
 });
