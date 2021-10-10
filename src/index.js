2ebbc2a5801f8dddf27c557c012b668c57da3c33
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-10 16:15:03 +0100

    Redux installed , reducer index setup , store setup imported to the index.js file and redux provider imported and used in the application

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+import { Provider } from 'react-redux';
+import store from '../store';
+
 ReactDOM.render(
+    <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
