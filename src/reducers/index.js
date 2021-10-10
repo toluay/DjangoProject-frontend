2732004932e9d4102e3165a50321eeee80d57bd0
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-10 18:15:45 +0100

    index reducer included auth

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import customers from './customers';
 import errors from './errors';
 import messages from './messages';
+import auth from './auth';
 
 
 export default combineReducers({
     customers,
     errors,
     messages,
+    auth,
 
 });
